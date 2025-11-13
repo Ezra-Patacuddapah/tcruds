0007c7dfcb64d94a677950af831e10886aea40f4
Ezra Patacuddapah <ezrapatacuddapah@gmail.com>
2025-11-13 16:06:37 +0300

    added cancel buttons

--- app/ui/admin/create-form.tsx
+++ app/ui/admin/create-form.tsx
@@ -2,11 +2,18 @@
 
 import { Button } from '@/app/ui/button'
 import { createText } from "@/app/lib/actions"
+import Link from 'next/link'
 
 export default function Form() {
     return (
         <form action={createText} className='flex justify-center items-center'>
             <input type="text" name="text" id="text" className='pl-2 border border-white rounded-l-md' />
+            <Link
+                href='/admin'
+                className='bg-gray-600 p-1'
+            >
+                Cancel
+            </Link>
             <Button type="submit">Create</Button>
         </form>
     )
--- app/ui/admin/update-form.tsx
+++ app/ui/admin/update-form.tsx
@@ -3,12 +3,19 @@
 import { Button } from '../button'
 import { updateText } from '@/app/lib/actions'
 import { Text } from '@/app/lib/definitions'
+import Link from 'next/link'
 
 export default function UpdateTextForm({ text }: { text: Text} ) {
     const updateTextWithId = updateText.bind(null, text.id)
     return (
         <form action={updateTextWithId} className='flex justify-center items-center'>
             <input type="text" name="text" id="text" defaultValue={text.text} className='pl-2 border border-white rounded-l-md' />
+            <Link
+                href='/admin'
+                className='bg-gray-600 p-1'
+            >
+                Cancel
+            </Link>
             <Button type="submit">Update</Button>
         </form>
     )
